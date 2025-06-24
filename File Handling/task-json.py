7185655385d2170e5ef9e55b3f5fe5292c7025bc
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-06-24 15:56:42 +0530

    task-json.py

--- File Handling/task-json.py
+++ File Handling/task-json.py
@@ -46,19 +46,3 @@ data = {
 json_string = json.dumps(data, indent=2)
 print(json_string)
 
-
-import json
-
-try:
-    with open('data.json', 'r') as file:
-        data = json.load(file)
-        print(data)
-
-except FileNotFoundError:
-    print("The file was not found.")
-
-except json.JSONDecodeError:
-    print("Invalid JSON format.")
-
-except Exception as e:
-    print("An unexpected error occurred:", e)
