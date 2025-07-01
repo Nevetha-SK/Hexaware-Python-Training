61d1d43cb6e43b6bacc0bcfa3f850d241b60857a
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-07-01 14:52:37 +0530

    Delete Python/Case Study-ProjectManagement/entity/Task.py

--- Python/Case Study-ProjectManagement/entity/Task.py
+++
@@ -1,9 +0,0 @@
-# entity/Task.py
-class Task:
-    def __init__(self, task_id=None, task_name=None, project_id=None,
-                 employee_id=None, status=None):
-        self.task_id = task_id
-        self.task_name = task_name
-        self.project_id = project_id
-        self.employee_id = employee_id
-        self.status = status
