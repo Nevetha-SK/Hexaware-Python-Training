bd26222e0a5a47f8c31d683082f91aecc56806c4
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-07-01 14:52:20 +0530

    Delete Python/Case Study-ProjectManagement/entity/Project.py

--- Python/Case Study-ProjectManagement/entity/Project.py
+++
@@ -1,8 +0,0 @@
-# entity/Project.py
-class Project:
-    def __init__(self, id=None, project_name=None, description=None, start_date=None, status=None):
-        self.id = id
-        self.project_name = project_name
-        self.description = description
-        self.start_date = start_date
-        self.status = status
