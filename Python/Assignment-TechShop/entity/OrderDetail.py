9d42057253c281860d74e9c9f4aaab1d7ff3abfc
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-07-02 14:57:44 +0530

    Create OrderDetail.py

---
+++ Python/Assignment-TechShop/entity/OrderDetail.py
@@ -0,0 +1,6 @@
+class OrderDetail:
+    def __init__(self, order_detail_id, order_id, product_id, quantity):
+        self.order_detail_id = order_detail_id
+        self.order_id = order_id
+        self.product_id = product_id
+        self.quantity = quantity
