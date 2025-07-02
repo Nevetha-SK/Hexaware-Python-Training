c14b8617dd8acf9d5f7ee165be27503aad8f8ca7
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-07-02 14:55:08 +0530

    Create Product.py

---
+++ Python/Assignment-TechShop/entity/Product.py
@@ -0,0 +1,6 @@
+class Product:
+    def __init__(self, product_id, name, description, price):
+        self.product_id = product_id
+        self.name = name
+        self.description = description
+        self.price = price
