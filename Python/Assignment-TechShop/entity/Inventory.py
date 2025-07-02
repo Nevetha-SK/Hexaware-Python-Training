5a5759202589a77a13857c798657c4655fd21167
Nevetha SK <163834746+Nevetha-SK@users.noreply.github.com>
2025-07-02 14:56:23 +0530

    Create Inventory.py

---
+++ Python/Assignment-TechShop/entity/Inventory.py
@@ -0,0 +1,5 @@
+class Inventory:
+    def __init__(self, product_id, quantity_in_stock, last_updated):
+        self.product_id = product_id
+        self.quantity_in_stock = quantity_in_stock
+        self.last_updated = last_updated
